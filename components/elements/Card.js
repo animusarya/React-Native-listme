5761e7c85b1884a232198047bfba0ae3eaeb1fb8
sourav arya <souravarya4u@gmail.com>
2019-06-18 12:27:40 +0530

    added bg img in card

--- components/elements/Card.js
+++ components/elements/Card.js
@@ -3,39 +3,32 @@ import styled from 'styled-components';
 
 // import { Ionicons } from '@expo/vector-icons';
 
-import bgimg from '../../assets/images/red.jpg';
 import { ImageBackground, Text } from 'react-native';
 
-const MainContainer = styled.View`
-`;
-
-
-
-
-const CardWrapper = styled.View`
+const CardImageBackground = styled(ImageBackground)`
+  width : 300;
+  height: 300;
+  align-self: center;
 `;
 
 const Title = styled.Text`
-    font-size : 35; 
-   
-`;
-const SubTitle = styled.Text`
-   
+    font-size : 21; 
+    margin-top: 80%;
+    font-weight : 600;
+    color: white;
 `;
 
-const CardImage = () => (
-    // <MainContainer>
-    //     <CardImageWrapper source={bgimg} />
-    //     <CardWrapper>
-    //         {/* <Ionicons name="md-more-vertical" size={32} color="green" /> */}
-    //         <Title>London Text</Title>
-    //         <SubTitle>Demo Text</SubTitle>
-    //     </CardWrapper>
-    // </MainContainer>
-    <ImageBackground source={bgimg} style={{ width: '200%', height: '300%' }}>
-        <Title>Lodon trip</Title>
-        <SubTitle>130 of 220</SubTitle>
-    </ImageBackground>
+const SubTitle = styled.Text`
+    font-size : 19;  
+    color: white;
+    font-weight : 600;
+`;
+
+const CardImage = ({ title, subtitle, backgroundImg }) => (
+    <CardImageBackground source={backgroundImg}>
+        <Title>{title}</Title>
+        <SubTitle>{subtitle}</SubTitle>
+    </CardImageBackground>
 );
 
 export default CardImage;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, } from 'react-native';
 import Card from '../components/elements/Card';
 
-export default function HomeScreen() {
-  return (
-    <ScrollView>
-      <Card />
-    </ScrollView>
-  );
-}
+import bgimg from '../assets/images/secondgril.jpg';
 
+
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Home"
+  };
+  render() {
+    return (
+      <ScrollView>
+        <Card title="London trip" subtitle="130£ to 150£" backgroundImg={bgimg} />
+        <Card title="London trip" subtitle="130£ to 150£" backgroundImg={bgimg} />
+      </ScrollView>
+    );
+  }
+}
